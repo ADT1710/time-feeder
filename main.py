40951f7920afc229a1d1689b88ef17f63ffe3edf
adt1710 <adrianoibira.adt@gmail.com>
2022-10-12 09:39:26 -0300

    Fazendo a diferença e iterador de data

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from datetime import date, datetime
+from datetime import datetime, timedelta, time, date
 
-data_inicial = datetime.strftime(datetime.strptime('01/01/2000', '%d/%m/%Y'),'%d/%m/%Y')
-data_final = datetime.strftime(datetime.strptime('31/12/2022', '%d/%m/%Y'),'%d/%m/%Y')
+data_inicial = datetime.strptime('01/01/2000', '%d/%m/%Y')
+data_final = datetime.strptime('31/12/2022', '%d/%m/%Y')
 
+diferenca = (data_final - data_inicial).days
+
+for dia in range(diferenca):
+    print(data_inicial + timedelta(dia+1))
