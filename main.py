5a99e5172e294689f2baf8437971bd7a5e4c4f12
adt1710 <adrianoibira.adt@gmail.com>
2022-10-12 09:24:49 -0300

    Aplicando datetime às variáveis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
 
-data_inicial = '01/01/2000'
-data_final = '31/12/2022'
+data_inicial = datetime.strftime(datetime.strptime('01/01/2000', '%d/%m/%Y'),'%d/%m/%Y')
+data_final = datetime.strftime(datetime.strptime('31/12/2022', '%d/%m/%Y'),'%d/%m/%Y')
 
