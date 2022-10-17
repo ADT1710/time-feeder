d99bfe12f773d6f3d01e548c9aa45bc7784f4628
adt1710 <adrianoibira.adt@gmail.com>
2022-10-17 07:06:55 -0300

    Função para determinar o dia da semana em PT-BR

---
+++ define_dia_semana.py
@@ -0,0 +1,3 @@
+def define_dia_semana(dia):
+    dias_da_semana = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo']
+    return dias_da_semana[dia]
