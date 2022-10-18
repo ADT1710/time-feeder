d00ed9b423f4d5192b0ad5dbf610911f585ef9bc
adt1710 <adrianoibira.adt@gmail.com>
2022-10-18 10:22:06 -0300

    Função de definição do nome do mês em PT-BR

---
+++ define_mes_nome.py
@@ -0,0 +1,3 @@
+def define_mes_nome(mes):
+    nomes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+    return nomes[mes - 1]
