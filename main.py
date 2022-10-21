b4f7c92fb4be054d68ea68310e4eb82b2955de61
adt1710 <adrianoibira.adt@gmail.com>
2022-10-21 09:02:56 -0300

    Adicionando implementação da PyMySQL.

--- main.py
+++ main.py
@@ -11,27 +11,38 @@ data_final = datetime.strptime('31/12/2022', '%d/%m/%Y')
 
 diferenca = (data_final - data_inicial).days
 
-for dia in range(diferenca):
-    data = data_inicial + timedelta(dia+1)
+conexao = pymysql.connect(host='localhost',
+                          user='root',
+                          password='passtest',
+                          database ='db'
+                          )
+with conexao:
+    with conexao.cursor() as cursor:
+        for dia in range(diferenca):
+            data = data_inicial + timedelta(dia+1)
 
-    dia_semana = define_dia_semana(data.weekday())
-    semana = define_semana(data.day)
+            dia_semana = define_dia_semana(data.weekday())
+            semana = define_semana(data.day)
 
-    mes_num = data.month
-    mes_nome = define_mes_nome(mes_num)
+            mes_num = data.month
+            mes_nome = define_mes_nome(mes_num)
 
-    ano = data.year
+            ano = data.year
 
-    trimestre_num = define_trimestre_num(mes_num)
-    trimestre_nome = define_trimestre_nome(trimestre_num)
+            trimestre_num = define_trimestre_num(mes_num)
+            trimestre_nome = define_trimestre_nome(trimestre_num)
 
-    semestre_num = define_semestre_num(int(trimestre_num))
-    semestre_nome = define_semestre_nome(semestre_num)
+            semestre_num = define_semestre_num(int(trimestre_num))
+            semestre_nome = define_semestre_nome(semestre_num)
 
-    cod_dia = data.strftime('%Y%m%d')
-    dia_mes = data.strftime('%m%d')
-    mes_ano = data.strftime('%Y%m')
+            cod_dia = data.strftime('%Y%m%d')
+            dia_mes = data.strftime('%m%d')
+            mes_ano = data.strftime('%Y%m')
 
-    data_formatada = data.strftime("%Y-%m-%d")
+            data_formatada = data.strftime("%Y-%m-%d")
 
-    print(f'{cod_dia} | {dia_mes} | {mes_ano} |{data_formatada} | {dia_semana} | {semana} | {mes_num} - {mes_nome} | {trimestre_num} - {trimestre_nome} | {semestre_num} | {semestre_nome}')
+            print(f'{cod_dia} | {dia_mes} | {mes_ano} |{data_formatada} | {dia_semana} | {semana} | {mes_num} - {mes_nome} | {trimestre_num} - {trimestre_nome} | {semestre_num} | {semestre_nome}')
+
+            sql = f'insert into dim_tempo (cod_dia, dia_mes, mes_ano, data_formatada, dia_semana, semana, mes_num, mes_nome, trimestre_num, trimestre_nome, semestre_num, semestre_nome) values ("{cod_dia}","{dia_mes}","{mes_ano}","{data_formatada}","{dia_semana}","{semana}","{mes_num}","{mes_nome}","{trimestre_num}","{trimestre_nome}","{semestre_num}","{semestre_nome}")'
+            cursor.execute(sql)
+    conexao.commit()
